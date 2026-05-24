39ade5c3f5b6686072851ff598b94f83488875c5
emerald32-source <emerald320032@gmail.com>
2026-05-24 23:17:14 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,49 @@
+const clientCustomName = "صياء";
+const clientGender = "m";
+const clientEidiyaCode = "EID-500-VIP";
+const clientEnvelopeText = `ضياء
+كل عام وانت بكل الخير  قلب عمتو🥰
+وعقبال اعياد كتيييير تعيشها في سعاده
+عيدك مباااارك❤️`;
+const clientMemories = [
+    {
+        "photo": "photo1.jpg",
+        "caption": "ابو ضحكه جنان"
+    },
+    {
+        "photo": "photo2.jpg",
+        "caption": "قلب عمتو"
+    },
+    {
+        "photo": "photo3.jpg",
+        "caption": "حبيب عمتو"
+    },
+    {
+        "photo": "photo4.jpg",
+        "caption": "ربي يحفظك"
+    },
+    {
+        "photo": "photo5.jpg",
+        "caption": "ربي يحميك"
+    },
+    {
+        "photo": "photo6.jpg",
+        "caption": "الراجل الجدع"
+    },
+    {
+        "photo": "photo7.jpg",
+        "caption": "عيدكم مبارك"
+    },
+    {
+        "photo": "photo8.jpg",
+        "caption": "يسعدك ربي"
+    },
+    {
+        "photo": "photo9.jpg",
+        "caption": "سيد الناس"
+    },
+    {
+        "photo": "photo10.jpg",
+        "caption": "اطيب قلب"
+    }
+];
